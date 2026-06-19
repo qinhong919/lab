d8d35b40e432d7b7a941b6c560c9dba89d5f221d
laodao <144201488+qinhong919@users.noreply.github.com>
2026-06-20 04:56:46 +0800

    Quantum.io_H5_v1
    Signed-off-by: laodao <144201488+qinhong919@users.noreply.github.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,30 @@
 
-async function loadDB(){
-  const res = await fetch('./data/db.json');
-  return await res.json();
+async function load(){
+  return await (await fetch('./data/db.json')).json();
 }
 
-async function renderIndex(){
-  const db = await loadDB();
+async function render(){
+  const db = await load();
   const box = document.getElementById('list');
 
+  const feature = db.articles.find(a=>a.id===db.featured);
+
+  document.getElementById("featureTitle").innerText =
+    feature.cn;
+
+  document.getElementById("featureEn").innerText =
+    feature.en;
+
   db.articles.forEach(a=>{
-    const el = document.createElement('div');
-    el.className='card';
-    el.innerHTML=`
+    const div = document.createElement('div');
+    div.className="card";
+    div.innerHTML=`
       <span class="badge">${a.type}</span>
-      <h3>${a.title_cn}</h3>
-      <div>${a.title_en}</div>
+      <h3>${a.cn}</h3>
+      <div>${a.en}</div>
       <p>${a.author}</p>
       <a href="${a.file}">阅读全文</a>
     `;
-    box.appendChild(el);
+    box.appendChild(div);
   });
 }
