ac83b5052b8d7fa8b1b0a4a49bb6161997ea260d
laodao <144201488+qinhong919@users.noreply.github.com>
2026-06-20 05:01:47 +0800

    Quantum.io_H5_v1
    Signed-off-by: laodao <144201488+qinhong919@users.noreply.github.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,26 +5,31 @@ async function load(){
 
 async function render(){
   const db = await load();
+
   const box = document.getElementById('list');
+  const side = document.getElementById('side');
+  const hero = db.articles.find(a=>a.id===db.featured);
 
-  const feature = db.articles.find(a=>a.id===db.featured);
-
-  document.getElementById("featureTitle").innerText =
-    feature.cn;
-
-  document.getElementById("featureEn").innerText =
-    feature.en;
+  document.getElementById("heroTitle").innerText = hero.cn;
+  document.getElementById("heroEn").innerText = hero.en;
 
   db.articles.forEach(a=>{
-    const div = document.createElement('div');
-    div.className="card";
-    div.innerHTML=`
+    const d = document.createElement('div');
+    d.className="card";
+    d.innerHTML=`
       <span class="badge">${a.type}</span>
       <h3>${a.cn}</h3>
       <div>${a.en}</div>
       <p>${a.author}</p>
       <a href="${a.file}">阅读全文</a>
     `;
-    box.appendChild(div);
+    box.appendChild(d);
   });
+
+  side.innerHTML = `
+    <h3>期刊信息</h3>
+    <p>Vol.1 No.1 2026</p>
+    <p>Open Access</p>
+    <p>国际超心理学与中国传统文化</p>
+  `;
 }
